c1f8b8e6d5d747f97b236999bf868cead431ccf9
Nicolas Legay <nicolas.legay.etu@univ-lille.fr>
2020-01-27 10:25:57 +0100

    deploy in heroku

---
+++ src/test/java/com/miage/altea/trainer_api/bo/PokemonTest.java
@@ -0,0 +1,16 @@
+package com.miage.altea.trainer_api.bo;
+
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.Embeddable;
+
+import static org.junit.Assert.assertNotNull;
+
+public class PokemonTest {
+
+    @Test
+    void pokemon_shouldBeAnEmbeddable(){
+        assertNotNull(Pokemon.class.getAnnotation(Embeddable.class));
+    }
+
+}
